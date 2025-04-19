62aa12434f0bd0c6ecb4c8ce8e0cccf18ff383ec
Diangondi03 <elguito2003@gmail.com>
2025-04-19 13:28:02 -0400

    Add new image assets for game elements
    - Added ghost.png for ghost character representation.
    - Added grass.png for terrain visuals.
    - Added ground.png for ground texture.
    - Added ice.png for ice surface representation.
    - Added normal.png for normal map texture.
    - Added poison.png for poison effect visuals.
    - Added psychic.png for psychic element representation.
    - Added rock.png for rock texture.
    - Added steel.png for steel material representation.
    - Added water.png for water surface visuals.

--- src/app/pokemons/_components/PokemonCard.tsx
+++ src/app/pokemons/_components/PokemonCard.tsx
@@ -1,22 +1,27 @@
+import Image from 'next/image'
+import Link from 'next/link'
 import React from 'react'
 
 const PokemonCard = ({pokemon}:{pokemon:any}) => {
 
     return (
-        <div className="card bg-accent shadow-xl transition-all duration-300 ease-in-out hover:scale-105 hover:shadow-2xl hover:-translate-y-1">
+        <Link href="" className='m-0 p-0'>
+        <div className="card bg-accent shadow-xl transition-all duration-300 ease-in-out hover:scale-105 hover:shadow-2xl hover:-translate-y-1 border-1 border-gray-500 h-full">
             <figure className="px-10 pt-10">
             <img src={pokemon.sprites.front_default} alt={pokemon.name} className="rounded-xl w-32 h-32 object-contain" />
             </figure>
             <div className="card-body items-center text-center p-4">
             <h2 className="card-title capitalize">{pokemon.name} #{pokemon.id}</h2>
             <div className="card-actions justify-center mt-2 flex-wrap items-center">
-            <span className="font-semibold mr-2 w-full text-center sm:w-auto sm:text-left mb-1 sm:mb-0">Types:</span>
-            {pokemon.types.map((typeInfo: { type: { name: string } }) => (
-            <div key={typeInfo.type.name} className="badge badge-primary badge-outline capitalize m-1">{typeInfo.type.name}</div>
+            {pokemon.types.map((typeInfo: { type: { name: string } },index) => (
+                <div key={index}>
+            <Image src={`/types/${typeInfo.type.name}.png`} width={20} height={20} alt={typeInfo.type.name}  className="w-8 h-8 m-1" /> 
+                </div>
             ))}
             </div>
             </div>
         </div>
+        </Link>
     )
 }
 
--- src/app/pokemons/_components/PokemonList.tsx
+++ src/app/pokemons/_components/PokemonList.tsx
@@ -1,8 +1,15 @@
-import React from 'react'
+"use client"
+import React, { useEffect, useState } from 'react'
 import PokemonCard from './PokemonCard'
+import { useInView } from 'react-intersection-observer'
 
-const PokemonList = async () => {
-    const getPokemons = async (id:number) => {
+const NUMBER_OF_POKEMONS = 24; // Number of pokemons to fetch
+
+const PokemonList =  () => {
+    const [offset, setOffset] = useState(1)
+    const [pokemons, setPokemons] = useState([])
+    const { ref, inView } = useInView()
+    const getPokemon = async (id:number) => {
         const url = `https://pokeapi.co/api/v2/pokemon/${id}/`; // Construct the URL using the ID
         const response = await fetch(url);
     
@@ -14,18 +21,37 @@ const PokemonList = async () => {
         const data = await response.json();
         return data
     }
+    const getPokemons = async()=>{
 
+        const indexes = Array.from({length: pokemons.length<=1025-NUMBER_OF_POKEMONS ? NUMBER_OF_POKEMONS : 1025-pokemons.length}, (_, i) => i + offset); // Create an array of indexes from offset to offset + NUMBER_OF_POKEMONS
+        const pokemonPromises = indexes.map((index) => getPokemon(index)); // Fetching 100 pokemons
+        const res = await Promise.all(pokemonPromises); // Wait for all promises to resolve
+        return res
+    }
 
-    const pokemonPromises = [...Array(20)].map((_, index) => getPokemons(index + 1)); // Fetching 100 pokemons
-    const pokemons = await Promise.all(pokemonPromises); // Wait for all promises to resolve
+    const loadMorePokemon = async () => {
+        if (pokemons.length >= 1025) return // If all pokemons are loaded, do nothing
+        const apiPokemons = await getPokemons(offset, NUMBER_OF_POKEMONS)
+        setPokemons(pokemons => [...pokemons, ...apiPokemons])
+        setOffset(offset => offset + NUMBER_OF_POKEMONS)
+    }
+
+    useEffect(() => {
+        if (inView) {
+          loadMorePokemon()
+        }
+    }, [inView])
+
 
     return (
         <>
                 <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4 p-4">
-                    {/* Replicating the card 12 times for demonstration */}
                     {pokemons.map((pokemon, index) => (
                         <PokemonCard key={index} pokemon={pokemon} />
                     ))}
+                </div>
+                <div ref={ref} className={`${pokemons.length >= 1025 ? 'hidden' : 'flex justify-center items-center h-16 my-10'}`}>
+                    <span className="loading loading-spinner loading-xl"></span>
                 </div>
         </>
     )
